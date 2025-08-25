a1bd35628cb6072a93b4c7fd088f8f0e130e4ac3
dariia <dariia.vengrzhanovska@bequalified.de>
2025-08-25 14:21:57 +0200

    use ngOnInit

--- course-booking-system/src/app/courses-list/courses-list.ts
+++ course-booking-system/src/app/courses-list/courses-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-courses-list',
@@ -6,12 +6,16 @@ import { Component } from '@angular/core';
   templateUrl: './courses-list.html',
   styleUrl: './courses-list.css'
 })
-export class CoursesList {
+export class CoursesList implements OnInit {
 title: string = "Available courses";
 courses = [
     { id: 1, title: 'Intro to Angular', description: 'Learn the basics of Angular', price: 49, date: '2025-03-01', soldOut: false, img: 'angular-logo.png' },
     { id: 2, title: 'Advanced Angular', description: 'Deep dive into Angular internals', price: 99, date: '2025-04-10', soldOut: true, img: 'angular-logo.png'},
   ];
+
+  ngOnInit(): void {
+    console.log("CoursesList initialized!")
+  }
 
   viewDetails(title: string): void{
     alert(`Viewing details for ${title}`);
