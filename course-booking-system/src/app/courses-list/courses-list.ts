2aeff06c595d8361dbeaf2d838dd2126a19aa254
dariia <dariia.vengrzhanovska@bequalified.de>
2025-08-25 13:12:43 +0200

    add event binding

--- course-booking-system/src/app/courses-list/courses-list.ts
+++ course-booking-system/src/app/courses-list/courses-list.ts
@@ -9,7 +9,11 @@ import { Component } from '@angular/core';
 export class CoursesList {
 title: string = "Available courses";
 courses = [
-    { id: 1, title: 'Intro to Angular', description: 'Learn the basics of Angular', price: 49, date: '2025-03-01' },
-    { id: 2, title: 'Advanced Angular', description: 'Deep dive into Angular internals', price: 99, date: '2025-04-10' },
+    { id: 1, title: 'Intro to Angular', description: 'Learn the basics of Angular', price: 49, date: '2025-03-01', soldOut: false, img: 'angular-logo.png' },
+    { id: 2, title: 'Advanced Angular', description: 'Deep dive into Angular internals', price: 99, date: '2025-04-10', soldOut: true, img: 'angular-logo.png'},
   ];
+
+  viewDetails(title: string): void{
+    alert(`Viewing details for ${title}`);
+  }
 }
