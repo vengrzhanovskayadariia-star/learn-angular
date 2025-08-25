d3e8a0c4b49a2006a16c2d37b9761f9f03d37fbd
dariia <dariia.vengrzhanovska@bequalified.de>
2025-08-25 12:11:47 +0200

    understanding data binding

--- course-booking-system/src/app/courses-list/courses-list.ts
+++ course-booking-system/src/app/courses-list/courses-list.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './courses-list.css'
 })
 export class CoursesList {
-
+title: string = "Available courses";
+courses = [
+    { id: 1, title: 'Intro to Angular', description: 'Learn the basics of Angular', price: 49, date: '2025-03-01' },
+    { id: 2, title: 'Advanced Angular', description: 'Deep dive into Angular internals', price: 99, date: '2025-04-10' },
+  ];
 }
